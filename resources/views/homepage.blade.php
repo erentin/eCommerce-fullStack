9203fd9a02736cfc2dbdd21de35e79b9f4215636
talhaoguz <toguzz1@outlook.com>
2023-05-05 00:12:43 +0300

    homepage changed

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,11 +1,22 @@
 <x-app-layout>
-    <div class="w-full bg-white h-4/5">
+    <div class="w-full h-full bg-white">
         <div class="grid w-full grid-cols-1 grid-rows-2 md:grid-cols-2 md:grid-rows-1 gap-y-4 md:gap-0">
-            <div class="w-full h-full ">
-                <img class="w-full h-full" src="https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/w_441,c_limit/8178f277-0754-4ee0-a419-9fa79840019a/resmi-nike-sitesi.jpg" />
+            <div class="w-full h-full cursor-pointer">
+                <a>
+                    <img class="w-full h-full" src="https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/w_441,c_limit/8178f277-0754-4ee0-a419-9fa79840019a/resmi-nike-sitesi.jpg" />
+                    <div class="absolute top-[56%] left-[20%] h-20 ">
+                        <img class="w-full h-full " src="https://pngimg.com/d/nike_PNG20.png" />
+                    </div>
+                </a>
+
             </div>
-            <div class="relative w-full h-full ">
-                <img class="w-full h-full" src="https://www.soccerbible.com/media/124572/adi-3-min.jpg" />
+            <div class="relative w-full h-full cursor-pointer">
+                <a>
+                    <img class="w-full h-full" src="https://www.soccerbible.com/media/124572/adi-3-min.jpg" />
+                    <div class="absolute top-[20%] left-[30%] h-60 ">
+                        <img class="w-full h-full " src="https://www.freepnglogos.com/uploads/adidas-logo-png-26.png" />
+                    </div>
+                </a>
 
             </div>
 
