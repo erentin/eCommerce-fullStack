2b9e7dbe769cfcd46b57358bc825ce9b9db15279
talhaoguz <toguzz1@outlook.com>
2023-05-04 23:57:31 +0300

    homepage changed

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,10 +1,10 @@
 <x-app-layout>
     <div class="w-full bg-white h-4/5">
-        <div class="grid w-full grid-cols-2 ">
-            <div class="w-full h-full transition-all hover:scale-105">
+        <div class="grid w-full grid-cols-1 grid-rows-2 md:grid-cols-2 md:grid-rows-1 gap-y-4 md:gap-0">
+            <div class="w-full h-full ">
                 <img class="w-full h-full" src="https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/w_441,c_limit/8178f277-0754-4ee0-a419-9fa79840019a/resmi-nike-sitesi.jpg" />
             </div>
-            <div class="relative w-full h-full transition-all hover:scale-105">
+            <div class="relative w-full h-full ">
                 <img class="w-full h-full" src="https://www.soccerbible.com/media/124572/adi-3-min.jpg" />
 
             </div>
