b65427ea39da7d525fd7b72f18269280f0199fff
Eren Tın <erentin@outlook.com>
2023-05-11 11:41:20 +0300

    first commit

--- README.md
+++ README.md
@@ -67,3 +67,4 @@ The Laravel framework is open-sourced software licensed under the [MIT license](
 # ecommerce
 # ecommerce
 # ecommerce
+# sneaks-up-eCommerce
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,50 +1,31 @@
 <x-app-layout>
-
     <div class="w-full h-full bg-white">
         <div class="grid w-full h-full grid-cols-1 grid-rows-2 lg:grid-cols-2 lg:grid-rows-1 gap-y-4 lg:gap-0">
             <div class="relative w-full h-full cursor-pointer">
                 <a href="/categories/nike">
                     <div class="relative w-full h-full bg-black-rgba">
                         <div class="absolute top-0 left-0 z-40 w-full h-full bg-black-rgba">
-
                         </div>
                         <img class="top-0 left-0 w-full h-full " src="https://static.nike.com/a/images/f_auto/dpr_2.0,cs_srgb/w_441,c_limit/8178f277-0754-4ee0-a419-9fa79840019a/resmi-nike-sitesi.jpg" />
-
                     </div>
-
-
                     <div class="absolute  top-[50%] left-[35%] h-20 z-50 w-60">
                         <img class="w-full h-full " src="https://pngimg.com/d/nike_PNG20.png" />
                     </div>
                 </a>
-
             </div>
             <div class="relative w-full h-full cursor-pointer">
                 <a href="/categories/adidas">
                     <div class="relative w-full h-full ">
                         <div class="absolute top-0 left-0 z-40 w-full h-full bg-black-rgba">
-
                         </div>
                         <img class="top-0 left-0 w-full h-full " src="https://www.soccerbible.com/media/124572/adi-3-min.jpg" />
-
-
                     </div>
                     <div class="absolute top-[45%] left-[35%] h-40 w-60 z-50">
                         <img class="w-full h-full " src="https://www.freepnglogos.com/uploads/adidas-logo-20.png
 " />
                     </div>
                 </a>
-
-
-
             </div>
-
         </div>
-
-
-
-
-
     </div>
-
 </x-app-layout>
